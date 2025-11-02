8ad80ad6ea958518522697d44e8a7154f7af0f54
Devansh1-tech <guptadevansh470@gamil.com>
2025-11-02 11:16:10 +0530

    Update build files

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /home/himanshu/UNIHUB-SIMPLE/src/app/layout.tsx
+// File: D:\Coding\Uni Hub\UNIHUB\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
