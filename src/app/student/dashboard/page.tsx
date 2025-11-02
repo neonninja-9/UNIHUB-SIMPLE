60bcd109d12ce5b14760565ee21d1efb4c06062c
Devansh1-tech <guptadevansh470@gamil.com>
2025-11-02 11:41:52 +0530

    updated

--- src/app/student/dashboard/page.tsx
+++ src/app/student/dashboard/page.tsx
@@ -551,7 +551,6 @@ export default function StudentDashboard() {
               <MobileEngagementCard
                 attendance={attendance}
                 courses={courses}
-                pendingTasks={pendingTasks}
               />
               <StatsCards
                 grades={grades}
